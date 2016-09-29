e77ba533d4c2aef65f7094744f5ea9feb321b69a
nickruta <nickruta@gmail.com>
2016-09-29 15:48:44 -0400

    added a test to determine results based on a one element array

--- test_binsearch.py
+++ test_binsearch.py
@@ -4,7 +4,10 @@ import numpy as np
 
 # first test to verify that an empty array returns -1
 def test_empty_array():
-	assert binary_search([], 1) == -1 
+	assert binary_search([], 1) == -1
+
+def test_array_with_one_element():
+	assert binary_search([2], 2) == 0
 
 
 
